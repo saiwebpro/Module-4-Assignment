fb8b6542a633e071124927888281c95f2c9acde7
Sai Kumar Bhimarasetty <64826210+saiwebpro@users.noreply.github.com>
2024-02-29 14:12:43 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 (function () {
 
-    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+    var names = ["Sai", "Jogendera", "Hoppins", "Jagadeesh", "katreena", "Teddy", "Shravani", "Shakeela", "Prabhas", "Jitendra"];
     
     for (var i = 0; i < names.length; i++) {
     
